b2c886df53d972447f40fb0a513e36a09baaf3d8
Joseph Moshood <gladstargtschool6@gmail.com>
2021-07-03 16:47:12 +0100

    Update utils.tsx

--- test/utils.tsx
+++ test/utils.tsx
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 import { Provider } from '../src'
 
-export const client = createClient('https://example.com', 'some.fake.key')
+export const client = createClient('https://imdvvfcbtrfewysxgrnr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNDMzNzg3MSwiZXhwIjoxOTI5OTEzODcxfQ.O7Ck0RFbXxsJ9BpxdAuey5O_MJcC_lHM06xDYKTbWxg')
 
 export const Wrapper: React.FC<{ children: any }> = ({ children }) => (
     <Provider value={client}>{children}</Provider>

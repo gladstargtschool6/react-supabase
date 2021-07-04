683d7217e9b36ad8a9466171c5d94b12bf0a082d
Joseph Moshood <gladstargtschool6@gmail.com>
2021-07-04 03:28:18 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  imdvvfcbtrfewysxgrnr.supabase.co,
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNDMzNzg3MSwiZXhwIjoxOTI5OTEzODcxfQ.O7Ck0RFbXxsJ9BpxdAuey5O_MJcC_lHM06xDYKTbWxg'
+)

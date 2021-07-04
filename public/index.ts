366ceb1ce0ad8d6d2963b54e223a8b1ba7fd52b6
Joseph Moshood <gladstargtschool6@gmail.com>
2021-07-04 17:31:49 +0100

    Update index.ts

--- public/index.ts
+++ public/index.ts
@@ -1,3 +1,21 @@
 export * from './context'
 export * from './hooks'
 export * from './types'
+import { useRealtime } from 'react-supabase'
+
+const Todos = () => {
+  const [result, reexecute] = useRealtime('todos')
+
+  const { data, fetching, error } = result
+
+  if (fetching) return <p>Loading...</p>
+  if (error) return <p>Oh no... {error.message}</p>
+
+  return (
+    <ul>
+      {data.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  )
+}
